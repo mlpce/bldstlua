e8388643b14b115dbaccef27839b2a75eabca868
mlpce <154090656+mlpce@users.noreply.github.com>
2025-01-17 17:54:06 +0000

    Add gccbld.sh build script

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # bldstlua
+
+## Repo manifest and source code repositories
+
+The source code repositories required to build, including this one (bldstlua)
+can be obtained using the google repo tool. See the manifest repository at
+https://github.com/mlpce/mnfstlua.
+
+## build/gccbld.sh
+
+Builds the console program lua.ttp with libcmini, slinput and tosbindl using
+the toolchain files configured for m68k-atari-mint-gcc. To run the script,
+first change to the 'build' directory before invoking ./gccbld.sh. The script
+expects the directory layout to be the same as that provided by the 'repo sync'.
+
+## Lua
+
+Lua is licensed under the MIT license. For information about Lua and to view
+its license see the [Lua website](https://www.lua.org/).
+
+## libcmini
+
+libcmini is licensed under the LGPL-2.1. For information about libcmini and to
+view its license see the [libcmini github project](https://github.com/freemint/libcmini).
+
+## m68k-atari-mint-gcc
+
+For information about m68k-atari-mint-gcc see
+[Vincent Rivière's m68k-atari-mint cross-tools website](http://vincent.riviere.free.fr/soft/m68k-atari-mint/).
---
+++ build/gccbld.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+set -eE
+trap 'echo Build failure' ERR
+
+# Toolchain int size (16 or 32)
+TC_INT_SIZE=16
+
+if [ ! -f ./gccbld.sh ]; then
+  echo "Error: Run in same directory as ./gccbld.sh"
+  exit 1
+fi
+
+BLDSTLUA_DIR=$(realpath $(pwd)/..)
+
+# Install directory
+BLDSTLUA_INSTALL_DIR=${BLDSTLUA_DIR}/install
+mkdir -p ${BLDSTLUA_INSTALL_DIR}
+
+# VARs for LIBCMINI installation locations
+LIBCMINI_INCLUDE_PATH=${BLDSTLUA_INSTALL_DIR}/libcmini/include
+LIBCMINI_LIBRARY_PATH=${BLDSTLUA_INSTALL_DIR}/libcmini/lib
+LIBCMINI_STARTUP_PATH=${BLDSTLUA_INSTALL_DIR}/libcmini/startup
+
+# Build libcmini
+build_libcmini() {
+  pushd ${BLDSTLUA_DIR}/../libcmini
+
+  make -C contrib/linea install
+  make clean
+  echo "CFLAGS+=-DSTDIO_MAP_NEWLINE" > Make.config.local
+  VERBOSE=yes make PREFIX_FOR_INCLUDE=${LIBCMINI_INCLUDE_PATH} \
+    PREFIX_FOR_LIB=${LIBCMINI_LIBRARY_PATH} \
+    PREFIX_FOR_STARTUP=${LIBCMINI_STARTUP_PATH} install
+
+  popd
+}
+
+# Build slinput
+build_slinput() {
+  pushd ${BLDSTLUA_DIR}/../slinput/build/tos/gcc
+  git clean -dfx
+
+  cmake --toolchain ./gcc${TC_INT_SIZE}.cmk \
+    -D CMAKE_BUILD_TYPE=MinSizeRel \
+    -D CMAKE_INSTALL_PREFIX=${BLDSTLUA_INSTALL_DIR} \
+    -D LIBCMINI_ENABLED=ON \
+    ../../..
+  make install
+
+  popd
+}
+
+# Build and install Lua. This will install the headers needed for tosbindl
+build_lua() {
+  pushd ${BLDSTLUA_DIR}/../lua/build/tos/gcc
+  git clean -dfx
+
+  cmake --toolchain ./gcc${TC_INT_SIZE}.cmk \
+    -D CMAKE_BUILD_TYPE=MinSizeRel \
+    -D CMAKE_INSTALL_PREFIX=${BLDSTLUA_INSTALL_DIR} \
+    -D LIBCMINI_ENABLED=ON \
+    -D MLPCE_ENABLED=ON \
+    -D MLPCE_DEBUGLIB_ENABLED=OFF \
+    -D MLPCE_IOLIB_ENABLED=OFF \
+    -D MLPCE_MATHLIB_ENABLED=OFF \
+    -D MLPCE_OSLIB_ENABLED=OFF \
+    -D MLPCE_UTF8LIB_ENABLED=OFF \
+    ../../..
+  make install
+
+  popd
+}
+
+# Build tosbindl
+build_tosbindl() {
+  pushd ${BLDSTLUA_DIR}/../tosbindl/build/tos/gcc
+  git clean -dfx
+
+  cmake --toolchain ./gcc${TC_INT_SIZE}.cmk \
+    -D CMAKE_BUILD_TYPE=MinSizeRel \
+    -D CMAKE_INSTALL_PREFIX=${BLDSTLUA_INSTALL_DIR} \
+    ../../..
+  make install
+
+  popd
+}
+
+# Now build Lua with tosbindl enabled
+build_lua_with_tosbindl() {
+  pushd ${BLDSTLUA_DIR}/../lua/build/tos/gcc
+
+  cmake -D MLPCE_TOSBINDL_ENABLED=ON \
+    ../../..
+  make VERBOSE=1 install
+
+  popd
+}
+
+build_libcmini
+build_slinput
+build_lua
+build_tosbindl
+build_lua_with_tosbindl
